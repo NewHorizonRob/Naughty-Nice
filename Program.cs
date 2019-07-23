6d0d39311d51ba87ce12d23c831bde6c1311a61f
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-23 18:41:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            eprac b = new eprac();
+            {
+                bool keepGoing = true;
+                while (keepGoing == true)
+                {
+                    Console.WriteLine("\n1) Add Nice Names");
+                    Console.WriteLine("2) Add Naughty Names");
+                    Console.WriteLine("3) Show Nice Names");
+                    Console.WriteLine("4) Show Naughty Names");
+                    Console.WriteLine("0) Exit");
+                    string choice = Console.ReadLine();
+
+                    if (choice == "1")
+                    {
+                        b.NaughtyNice(categories.nice);
+                    }
+                    else if (choice == "2")
+                    {
+                        b.NaughtyNice(categories.naughty);
+                    }
+                    else if (choice == "3")
+                    {
+                        b.ShowNiceKids();
+                    }
+                    else if (choice == "4")
+                    {
+                        b.ShowNauhghtyKids();
+
+                    }
+                    else if (choice == "0")
+                    {
+                        keepGoing = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your choice is not valid");
+                    }
+
+                }
+                //Console.ReadKey();
+            }
+        }
+    }
+}
+    
+
---
+++ eprac.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3enums
+{
+    class eprac
+    {
+        List<string> behaviors;
+        List<string> behaviors2;
+
+        public eprac()
+        {
+            behaviors = new List<string>();
+            behaviors2 = new List<string>();
+        }
+        public void NaughtyNice(categories jak)
+        {
+            if (jak == categories.nice)
+            {
+                Console.WriteLine("Add names to the nice list:");
+                try
+                {
+                    string nice = Console.ReadLine();
+                    behaviors.Add(nice);
+                }
+                catch
+                {
+                    Console.WriteLine("Enter in something valid");
+                }
+            }
+
+            if (jak == categories.naughty)
+            {
+                    Console.WriteLine("Add names to the naughty list:");
+                    try
+                    {
+                        string naughty = Console.ReadLine();
+                        behaviors2.Add(naughty);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Enter in something valid");
+                    }
+                }
+            }
+        
+        public void ShowNiceKids()
+        {
+            foreach(string nice in behaviors)
+            {
+                Console.Write($"{nice}");
+            }
+        }
+        public void ShowNauhghtyKids()
+        {
+            foreach(string naughty in behaviors2 )
+            {
+                Console.Write($"{naughty}");
+            }
+        }
+    }
+}
