d93c079ff165c4e13a67dde27ba50f950e0ec4c6
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 02:08:36 -0400

    Update eprac.cs

--- eprac.cs
+++ eprac.cs
@@ -7,58 +7,63 @@ using System.Threading.Tasks;
 namespace test3enums
 {
     class eprac
-    {
+    {   
+        #below two new lists that are made of strings are created for the naughty and nice list 
         List<string> behaviors;
         List<string> behaviors2;
 
         public eprac()
-        {
+        {   
+            # behaviors and behaviors2 are assinged to the new lists
             behaviors = new List<string>();
             behaviors2 = new List<string>();
         }
-        public void NaughtyNice(categories jak)
+        public void NaughtyNice(categories jak) #method for the adding names to each of the lists
         {
-            if (jak == categories.nice)
-            {
-                Console.WriteLine("Add names to the nice list:");
+            if (jak == categories.nice)# if the user choice is nice code runs a try catch to add names to the nice list
+
+            {                
+                Console.WriteLine("Add names to the nice list:"); #prints statement to the console for user to add names
+
                 try
                 {
-                    string nice = Console.ReadLine();
-                    behaviors.Add(nice);
+                    string nice = Console.ReadLine();#uses the enums from categories as a string 
+                    behaviors.Add(nice); #adds the user input to the nice list 
                 }
                 catch
                 {
-                    Console.WriteLine("Enter in something valid");
+                    Console.WriteLine("Enter in something valid"); #runs in case the user input is invalid 
                 }
             }
 
-            if (jak == categories.naughty)
+            if (jak == categories.naughty) # if the user choice is naughty, code runs a try catch to add names to the naughty list
+
             {
                     Console.WriteLine("Add names to the naughty list:");
                     try
                     {
-                        string naughty = Console.ReadLine();
-                        behaviors2.Add(naughty);
+                        string naughty = Console.ReadLine(); #uses the enums from categories as a string 
+                        behaviors2.Add(naughty);#adds the user input to the naughty list 
                     }
                     catch
                     {
-                        Console.WriteLine("Enter in something valid");
+                        Console.WriteLine("Enter in something valid");#runs in case the user input is invalid 
                     }
                 }
             }
         
-        public void ShowNiceKids()
+        public void ShowNiceKids() #method for showing the strings in the nice list
         {
-            foreach(string nice in behaviors)
+            foreach(string nice in behaviors) #loops through the list using foreach 
             {
-                Console.Write($"{nice}");
+                Console.Write($"{nice}"); #writes out each of the string to the console
             }
         }
-        public void ShowNauhghtyKids()
+        public void ShowNauhghtyKids()#method for showing the strings in the naughty list
         {
-            foreach(string naughty in behaviors2 )
+            foreach(string naughty in behaviors2 )#loops through the list using foreach 
             {
-                Console.Write($"{naughty}");
+                Console.Write($"{naughty}");#writes out each of the string to the console
             }
         }
     }
