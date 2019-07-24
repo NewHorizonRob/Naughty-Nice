a1735bc7ec82a8c4119bb9575a3be1011ff7989c
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 01:38:27 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,9 +12,10 @@ namespace test3enums
         {
             eprac b = new eprac();
             {
-                bool keepGoing = true;
-                while (keepGoing == true)
-                {
+                bool keepGoing = true; #assings the boolean value to true 
+                while (keepGoing == true) #while loop utilizing the boolean value 
+                {   
+                    # choices for the user to choose from when using the program or to exit the while loop
                     Console.WriteLine("\n1) Add Nice Names");
                     Console.WriteLine("2) Add Naughty Names");
                     Console.WriteLine("3) Show Nice Names");
@@ -24,28 +25,28 @@ namespace test3enums
 
                     if (choice == "1")
                     {
-                        b.NaughtyNice(categories.nice);
+                        b.NaughtyNice(categories.nice); #runs this line of code if user choice is "one"
                     }
                     else if (choice == "2")
                     {
-                        b.NaughtyNice(categories.naughty);
+                        b.NaughtyNice(categories.naughty); #runs this line of code if user choice is "two"
                     }
                     else if (choice == "3")
                     {
-                        b.ShowNiceKids();
+                        b.ShowNiceKids(); #runs this line of code if user choice is "three"
                     }
                     else if (choice == "4")
                     {
-                        b.ShowNauhghtyKids();
+                        b.ShowNauhghtyKids(); #runs this line of code if user choice is "four"
 
                     }
                     else if (choice == "0")
                     {
-                        keepGoing = false;
+                        keepGoing = false; #runs this line of code if user choice is "zero"
                     }
                     else
                     {
-                        Console.WriteLine("Your choice is not valid");
+                        Console.WriteLine("Your choice is not valid"); # runs this line of code if the user choice is invalid 
                     }
 
                 }
